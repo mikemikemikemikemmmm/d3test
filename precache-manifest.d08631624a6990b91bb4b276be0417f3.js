c61c5dad2cef956f632fd8ea3e0510410c7a4cbd
mikemikemikemikemmmm <36297097+mikemikemikemikemmmm@users.noreply.github.com>
2019-10-20 19:48:32 +0800

    first commit

---
+++ precache-manifest.d08631624a6990b91bb4b276be0417f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a26f8268c638a102167f5255ca3654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "498793910539b1ba1421",
+    "url": "/static/js/2.13142e2d.chunk.js"
+  },
+  {
+    "revision": "995a100e45d6b9997c65",
+    "url": "/static/js/main.21378cdd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fb175d1913cfebd69e2eb90c117dba7e",
+    "url": "/static/media/data.fb175d19.csv"
+  }
+]);
